e0e4c461955e425d096bfc14238fb719e9ba4320
Marcus Crowe <marcuscrowe2005@gmail.com>
2025-01-21 20:27:34 -0500

    First version of library

---
+++ BankLibrary/BankLibrary/HighInterestSavingsAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLibrary
+{
+    public class HighInterestSavingsAccount : SavingsAccount
+    {
+        public HighInterestSavingsAccount(string accountNumber, decimal initialBalanace, decimal interestRate) :
+            base(accountNumber, initialBalanace, interestRate)
+        {
+
+        }
+        public void ApplyBonusInterest(decimal bonusRate)
+        {
+            decimal bonusInterestPayment = this.Balance * bonusRate;
+            this.Deposit(bonusInterestPayment);
+        }
+    }
+}
